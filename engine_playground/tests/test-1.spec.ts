2b8fa436546aac05ffd3cb27112d2d2caf5b1f3a
Philipp Geirhos <philipp@geirhos.com>
2025-01-29 17:18:27 +0100

    added engine container

--- engine_playground/tests/test-1.spec.ts
+++ engine_playground/tests/test-1.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://engine.projekt-adler.eu/');
+  await page.goto('http://app/');
   await expect(page.locator('h2')).toContainText('Bitte loggen Sie sich ein.');
 });
