fbcbed823b57695010490553610b82dbf2900da4
Philipp Geirhos <philipp@geirhos.com>
2025-01-29 17:24:48 +0100

    fixed address of engine

--- engine_playground/tests/test-1.spec.ts
+++ engine_playground/tests/test-1.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://app/');
+  await page.goto('http://localhost:3000/');
   await expect(page.locator('h2')).toContainText('Bitte loggen Sie sich ein.');
 });
