8f201707934a714dd0b6c41b61956c97e8c14d98
Philipp Geirhos <philipp@geirhos.com>
2025-01-30 14:31:32 +0100

    chrome gpu test

--- engine_playground/tests/test-1.spec.ts
+++ engine_playground/tests/test-1.spec.ts
@@ -4,3 +4,8 @@ test('Can access the engine', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page.locator('h2')).toContainText('Bitte loggen Sie sich ein.');
 });
+
+test('Chrome GPU', async ({ page }) => {
+  await page.goto('chrome://gpu/');
+  await page.waitForTimeout(5000);
+});
