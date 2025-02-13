c5d74394ea06b5d0c0ed606a5f9d27357785f35a
Glutamat42 <markus.heck13396@gmx.de>
2025-02-13 17:09:24 +0100

    remove playground project

--- engine_playground/tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Can access the engine', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('h2')).toContainText('Bitte loggen Sie sich ein.');
-});
