94dc077e38bcb2b1b4a7a0711fa00c994dc3ca08
Glutamat42 <markus.heck13396@gmx.de>
2025-02-14 01:25:00 +0100

    include authoring tool to docker compose

--- authoring_tool/playwright.config.ts
+++ authoring_tool/playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://${process.env._DOMAIN}:8001`,
+    baseURL: `http://${process.env._URL_AUTHORING_TOOL}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
