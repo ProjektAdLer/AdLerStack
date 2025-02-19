6bb03463961c5a8ccde83a1a588b5cdbefdc9a32
Glutamat42 <markus.heck13396@gmx.de>
2025-02-19 19:25:38 +0100

    update playwright configs for projects requiring webgl

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -45,14 +45,15 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          headless: false,
+          args: [
+            '--ignore-gpu-blocklist',
+          ],
+        },
+      }
+    }
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
