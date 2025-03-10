045d54564b3ff1d5be72e2c9b325124b865ba729
philgei <philipp@geirhos.com>
2025-03-10 22:02:30 +0100

    re-enabled beforall block in engine test

--- game/tests/accessElement.spec.ts
+++ game/tests/accessElement.spec.ts
@@ -1,26 +1,27 @@
 import {expect} from '@playwright/test';
 import {test} from "./libs/enhanced_test";
+import {enrollInMoodleCourse} from "./libs/moodle_helpers";
 
 test.describe.serial("Access a Learning Element in 3D", () => {
 
-    // test.beforeAll(async ({request, resetEnvironment, uploadWorld}) => {
-    //     // request: Objekt von Playwright, das HTTP-Anfragen senden kann
-    //
-    //     // resetEnvironment: Funktion, die die Testumgebung zurücksetzt. Dazu haben wir WSL aufgesetzt
-    //
-    //     // uploadWorld: Funktion, die eine Welt hochlädt und Informationen darüber zurückgibt
-    //     await resetEnvironment();
-    //
-    //     const uploadedWorld = await uploadWorld('testwelt');
-    //
-    //     // Enroll student
-    //     await enrollInMoodleCourse(
-    //         request,
-    //         process.env._PLAYWRIGHT_USER_STUDENT_USERNAME!,
-    //         process.env._USER_STUDENT_PW!,
-    //         uploadedWorld.worldNameInLms
-    //     );
-    // });
+    test.beforeAll(async ({request, resetEnvironment, uploadWorld}) => {
+        // request: Objekt von Playwright, das HTTP-Anfragen senden kann
+
+        // resetEnvironment: Funktion, die die Testumgebung zurücksetzt. Dazu haben wir WSL aufgesetzt
+
+        // uploadWorld: Funktion, die eine Welt hochlädt und Informationen darüber zurückgibt
+        await resetEnvironment();
+
+        const uploadedWorld = await uploadWorld('testwelt');
+
+        // Enroll student
+        await enrollInMoodleCourse(
+            request,
+            process.env._PLAYWRIGHT_USER_STUDENT_USERNAME!,
+            process.env._USER_STUDENT_PW!,
+            uploadedWorld.worldNameInLms
+        );
+    });
 
     // empty test to trigge the beforeAll
     test('empty test', async () => {
