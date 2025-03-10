578f165973753c85cff2b51bbd45806e05efc6c8
philgei <philipp@geirhos.com>
2025-03-10 22:12:00 +0100

    added to test test

--- game/tests/accessElement.spec.ts
+++ game/tests/accessElement.spec.ts
@@ -22,4 +22,27 @@ test.describe.serial("Access a Learning Element in 3D", () => {
             uploadedWorld.worldNameInLms
         );
     });
+
+    // empty test to trigge the beforeAll
+    test('empty test', async () => {
+        expect(true).toBeTruthy();
+    });
+
+    test('Student can Acces first Learning Element in the Room', async ({ page }) => {
+        await page.goto('/');
+        await page.getByTestId('userName').click();
+        await page.getByTestId('userName').click();
+        await page.getByTestId('userName').fill('integration_test_student');
+        await page.getByTestId('password').click();
+        await page.getByTestId('password').fill('Student1234!1234');
+        await page.getByTestId('password').click();
+        await page.getByTestId('loginButton').click();
+        await page.getByRole('button').filter({ hasText: /^$/ }).nth(1).click();
+        await page.getByRole('button', { name: 'testwelt' }).click();
+        await page.getByRole('button', { name: 'Lernwelt öffnen!' }).click();
+        await page.getByTestId('rf__node-1').getByRole('button', { name: 'testraum' }).click();
+        await page.getByRole('button', { name: 'Lernraum betreten!' }).click();
+        await page.getByRole('button', { name: 'Weiter zum Lernraum' }).click();
+        await page.getByRole('button', { name: 'Weiter zum Lernraum' }).click();
+    });
 })
